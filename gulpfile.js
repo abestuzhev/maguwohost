bca6aac4a6786063d559b3f9627658bdb5c771be
Aleksey <aleksey.bestuzhev@gmail.com>
2016-05-20 06:02:49 +0300

    changes

---
+++ gulpfile.js
@@ -0,0 +1,53 @@
+//plugins for development
+var gulp = require('gulp'),
+    // rimraf = require('rimraf'),
+    sass = require('gulp-sass'),
+    // inlineimage = require('gulp-inline-image'),
+    prefix = require('gulp-autoprefixer'),
+    plumber = require('gulp-plumber'),
+    browserSync = require('browser-sync').create();
+    // concat = require('gulp-concat');
+
+//plugins for buld
+// var purify = require('gulp-purifycss'),
+//     uglify = require('gulp-uglify'),
+//     imagemint = require('gulp-imagemint'),
+//     pngquant = require('imagemint-pngquant'),
+//     csso = require('gulp-csso');
+
+//plugins for testing
+// var html5lint = require('gulp-html5-lint');
+
+var srcDir = 'src/';
+
+gulp.task('sass', function(){
+  return gulp.src(srcDir + 'scss/**/*.scss')
+    .pipe(plumber())
+    .pipe(sass())
+    .pipe(prefix('last 3 version'))
+    .pipe(gulp.dest('css/'))
+    .pipe(browserSync.stream())
+});
+
+gulp.task('html', function(){
+  return gulp.src('**/*.html')
+    .pipe(plumber())
+    .pipe(gulp.dest('**/*.html'))
+    .pipe(browserSync.stream())
+});
+
+gulp.task('browser-sync', function(){
+  browserSync.init({
+    port: 1337,
+    server: {
+      baseDir: ''
+    }
+  })
+});
+
+gulp.task('watch', function(){
+  gulp.watch(srcDir + 'scss/**/*.scss', ['sass'])
+  gulp.watch('**/*.html', ['html'])
+});
+
+gulp.task('default', ['sass', 'html', 'watch', 'browser-sync'])
