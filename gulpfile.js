d40a1666480752f062e936e4c4f985b3b77cfe22
Aleksey Bestuzhev <aleksey.bestuzhev@gmail.com>
2016-06-07 09:06:32 +0300

    changes

--- gulpfile.js
+++ gulpfile.js
@@ -1,47 +1,30 @@
 //plugins for development
 var gulp = require('gulp'),
-    // rimraf = require('rimraf'),
     sass = require('gulp-sass'),
-    // inlineimage = require('gulp-inline-image'),
     prefix = require('gulp-autoprefixer'),
     plumber = require('gulp-plumber'),
     livereload = require('gulp-livereload'),
-    // connect = require('gulp-connect');
     browserSync = require('browser-sync').create();
-    // concat = require('gulp-concat');
 
-//plugins for buld
-// var purify = require('gulp-purifycss'),
-//     uglify = require('gulp-uglify'),
-//     imagemint = require('gulp-imagemint'),
-//     pngquant = require('imagemint-pngquant'),
-//     csso = require('gulp-csso');
-
-//plugins for testing
-// var html5lint = require('gulp-html5-lint');
 
 var srcDir = 'src/';
 
 gulp.task('sass', function(){
   return gulp.src(srcDir + 'scss/**/*.scss')
     .pipe(sass().on('error', sass.logError))
-    // .pipe(plumber())
     .pipe(prefix('last 3 version'))
     .pipe(gulp.dest('css/'))
-    // .pipe(connect.reload());
     .pipe(browserSync.stream())
 });
 
 gulp.task('js', function(){
   return gulp.src(srcDir + 'js/**/*.js')
     .pipe(gulp.dest('js/'))
-    // .pipe(connect.reload());
     .pipe(browserSync.stream())
 });
 
 gulp.task('html', function(){
   return gulp.src('**/*.html')
-    // .pipe(connect.reload());
     .pipe(browserSync.stream())
 });
 
